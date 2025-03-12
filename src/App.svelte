3cdea664fd5c7877eab62a7df5d4ad4ca1ea7c8c
itool <itbaby@users.noreply.github.com>
2025-03-12 22:04:29 +0800

    试图加上菜单

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,52 @@
 <script lang="ts">
+  import "animate.css";
+  import "metismenu";
+  import jq from "jquery";
+  import { switchLanguage } from "./i18n";
+  import { _, locale } from "svelte-i18n";
+  import { isLoading } from "svelte-i18n";
+  import { onMount } from "svelte";
 
-  import { switchLanguage } from './i18n';
-  import { _, locale } from 'svelte-i18n';
-  import { isLoading } from 'svelte-i18n';
-  
   let userName = "World";
-  let currentLang: 'en' | 'zh' = ($locale as 'en' | 'zh') || 'zh';  
+  let currentLang: "en" | "zh" = ($locale as "en" | "zh") || "zh";
 
+  onMount(() => {
+    console.log('here')
+    jq("#metismenu").metisMenu();
+  });
 </script>
 
 {#if $isLoading}
   <p>Loading translations...</p>
 {:else}
   <main>
-    <h1>{$_('greeting', { values: { name: userName } })}</h1>
-    <input bind:value={userName} placeholder="Enter your name">
-    <button on:click={() => {
-      currentLang = currentLang === 'en' ? 'zh' : 'en';
-      switchLanguage(currentLang);
-    }}>
-      {$_('button.toggle')}
+    <ul id="metismenu">
+      <li class="mm-active">
+       <a class="has-arrow" href="#" aria-expanded="true">Menu 1</a>
+       <ul>
+       ...
+       </ul>
+      </li>
+      <li>
+       <a class="has-arrow" href="#" aria-expanded="false">Menu 2</a>
+       <ul>
+       ...
+       </ul>
+      </li>
+      ...
+      </ul>
+    <h1>{$_("greeting", { values: { name: userName } })}</h1>
+    <input bind:value={userName} placeholder="Enter your name" />
+    <button
+      on:click={() => {
+        currentLang = currentLang === "en" ? "zh" : "en";
+        switchLanguage(currentLang);
+      }}
+    >
+      {$_("button.toggle")}
     </button>
   </main>
 {/if}
 
 <style>
-
 </style>
