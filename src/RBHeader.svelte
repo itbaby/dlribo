b34e13f2d1524e05325e4ba8d228e3d5bad35664
itool <itbaby@users.noreply.github.com>
2025-03-16 16:47:00 +0800

    add switch language

--- src/RBHeader.svelte
+++ src/RBHeader.svelte
@@ -1,27 +1,22 @@
+<script lang="ts">
+  import { switchLanguage } from "./i18n";
+
+  let userName = "World";
+
+</script>
+
 <div data-uk-grid class="uk-grid-match">
   <div class="uk-width-expand@m">
-   <nav data-uk-sticky data-uk-dropnav="boundary: true; stretch: x; flip: false" class="uk-height-small ">
-  <ul class="uk-subnav uk-padding-small">
-    <li class="uk-active"><a href="/">行业</a></li>
-    <li>
-      <a href={'#'}>服务</a>
-      <div class="uk-dropdown">
-        <ul class="uk-nav uk-dropdown-nav">
-          <li class="uk-active"><a href="/">Active</a></li>
-          <li><a href="/item">Item</a></li>
-          <li class="uk-nav-header">Header</li>
-          <li><a href="/item1">Item</a></li>
-          <li><a href="/item2">Item</a></li>
-          <li class="uk-nav-divider"></li>
-          <li><a href="/item3">Item</a></li>
-        </ul>
-      </div>
-    </li>
-    <li>
-      <a href={'#'}>创新</a>
-      <div class="uk-dropdown">
-        <div class="uk-child-width-1-2" data-uk-grid>
-          <div>
+    <nav
+      data-uk-sticky
+      data-uk-dropnav="boundary: true; stretch: x; flip: false"
+      class="uk-height-small"
+    >
+      <ul class="uk-subnav uk-padding-small">
+        <li class="uk-active"><a href="/">行业</a></li>
+        <li>
+          <a href={"#"}>服务</a>
+          <div class="uk-dropdown">
             <ul class="uk-nav uk-dropdown-nav">
               <li class="uk-active"><a href="/">Active</a></li>
               <li><a href="/item">Item</a></li>
@@ -32,43 +27,54 @@
               <li><a href="/item3">Item</a></li>
             </ul>
           </div>
-          <div>
-            <ul class="uk-nav uk-dropdown-nav">
-              <li class="uk-active"><a href="/">Active</a></li>
-              <li><a href="/item">Item</a></li>
-              <li class="uk-nav-header">Header</li>
-              <li><a href="/item1">Item</a></li>
-              <li><a href="/item2">Item</a></li>
-              <li class="uk-nav-divider"></li>
-              <li><a href="/item3">Item</a></li>
-            </ul>
+        </li>
+        <li>
+          <a href={"#"}>创新</a>
+          <div class="uk-dropdown">
+            <div class="uk-child-width-1-2" data-uk-grid>
+              <div>
+                <ul class="uk-nav uk-dropdown-nav">
+                  <li class="uk-active"><a href="/">Active</a></li>
+                  <li><a href="/item">Item</a></li>
+                  <li class="uk-nav-header">Header</li>
+                  <li><a href="/item1">Item</a></li>
+                  <li><a href="/item2">Item</a></li>
+                  <li class="uk-nav-divider"></li>
+                  <li><a href="/item3">Item</a></li>
+                </ul>
+              </div>
+              <div>
+                <ul class="uk-nav uk-dropdown-nav">
+                  <li class="uk-active"><a href="/">Active</a></li>
+                  <li><a href="/item">Item</a></li>
+                  <li class="uk-nav-header">Header</li>
+                  <li><a href="/item1">Item</a></li>
+                  <li><a href="/item2">Item</a></li>
+                  <li class="uk-nav-divider"></li>
+                  <li><a href="/item3">Item</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </li>
-  </ul>
-  </nav>
+        </li>
+      </ul>
+    </nav>
   </div>
   <div class="uk-padding-small">
     <nav data-uk-dropnav>
-    <ul class="uk-subnav">
-        
+      <ul class="uk-subnav">
         <li class="uk-active">
-            <a href={'#'}>语言 <span data-uk-drop-parent-icon></span></a>
-            <div class="uk-dropdown">
-                <ul class="uk-nav uk-dropdown-nav">
-                    <li class="uk-active"><a href={"#"}>中文</a></li>
-                    <li><a href={"#"}>英文</a></li>
-                    <li><a href={"#"}>日语</a></li>
-                </ul>
-            </div>
+          <a href={"#"}>语言 <span data-uk-drop-parent-icon></span></a>
+          <div class="uk-dropdown">
+            <ul class="uk-nav uk-dropdown-nav">
+              <li class="uk-active"><a href={"#"} on:click={() => switchLanguage('zh')}>中文</a></li>
+              <li><a href={"#"}  on:click={() => switchLanguage('en') }>英文</a></li>
+              <li><a href={"#"}>日语</a></li>
+            </ul>
+          </div>
         </li>
-        
-    </ul>
-
-</nav>
-
-
+      </ul>
+    </nav>
   </div>
 </div>
 
